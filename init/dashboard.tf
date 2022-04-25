a37fc48fa29e237b7f020aedebacbe76ed856096
runalsh <dev@runalsh.ru>
2022-04-25 10:11:46 +0300

    fix

---
+++ init/dashboard.tf
@@ -0,0 +1,275 @@
+resource "aws_cloudwatch_dashboard" "eks-cluster-application" {
+  dashboard_name = "eks-cluster-application"
+  dashboard_body = <<EOF
+
+{
+    "widgets": [
+        {
+            "height": 6,
+            "width": 6,
+            "y": 0,
+            "x": 0,
+            "type": "metric",
+            "properties": {
+                "metrics": [
+                    [ { "id": "expr1m0", "label": "epam-py-cluster", "expression": "mm1m0 * 100 / mm0m0", "region": "eu-central-1" } ],
+                    [ "ContainerInsights", "node_cpu_limit", "ClusterName", "epam-py-cluster", { "period": 300, "stat": "Sum", "id": "mm0m0", "visible": false, "region": "eu-central-1" } ],
+                    [ "ContainerInsights", "node_cpu_usage_total", "ClusterName", "epam-py-cluster", { "period": 300, "stat": "Sum", "id": "mm1m0", "visible": false, "region": "eu-central-1" } ]
+                ],
+                "legend": {
+                    "position": "bottom"
+                },
+                "title": "CPU Utilization",
+                "yAxis": {
+                    "left": {
+                        "min": 0,
+                        "showUnits": false,
+                        "label": "Percent"
+                    }
+                },
+                "region": "eu-central-1",
+                "liveData": false,
+                "start": "-PT1H",
+                "end": "PT0H",
+                "view": "timeSeries",
+                "stacked": true,
+                "period": 300
+            }
+        },
+        {
+            "height": 6,
+            "width": 6,
+            "y": 0,
+            "x": 6,
+            "type": "metric",
+            "properties": {
+                "metrics": [
+                    [ { "id": "expr1m0", "label": "epam-py-cluster", "expression": "mm1m0 * 100 / mm0m0" } ],
+                    [ "ContainerInsights", "node_memory_limit", "ClusterName", "epam-py-cluster", { "period": 300, "stat": "Sum", "id": "mm0m0", "visible": false } ],
+                    [ "ContainerInsights", "node_memory_working_set", "ClusterName", "epam-py-cluster", { "period": 300, "stat": "Sum", "id": "mm1m0", "visible": false } ]
+                ],
+                "legend": {
+                    "position": "bottom"
+                },
+                "title": "MemoryUtilization",
+                "yAxis": {
+                    "left": {
+                        "min": 0,
+                        "showUnits": false,
+                        "label": "Percent"
+                    }
+                },
+                "region": "eu-central-1",
+                "liveData": false,
+                "start": "-PT1H",
+                "end": "PT0H",
+                "view": "timeSeries",
+                "stacked": true
+            }
+        },
+        {
+            "height": 6,
+            "width": 6,
+            "y": 0,
+            "x": 12,
+            "type": "metric",
+            "properties": {
+                "metrics": [
+                    [ "ContainerInsights", "node_network_total_bytes", "ClusterName", "epam-py-cluster", { "period": 300, "stat": "Average" } ]
+                ],
+                "legend": {
+                    "position": "bottom"
+                },
+                "title": "Network",
+                "region": "eu-central-1",
+                "liveData": false,
+                "start": "-PT1H",
+                "end": "PT0H",
+                "view": "timeSeries",
+                "stacked": true
+            }
+        },
+        {
+            "height": 3,
+            "width": 6,
+            "y": 0,
+            "x": 18,
+            "type": "metric",
+            "properties": {
+                "metrics": [
+                    [ "ContainerInsights", "cluster_node_count", "ClusterName", "epam-py-cluster", { "period": 300, "stat": "Average" } ]
+                ],
+                "legend": {
+                    "position": "bottom"
+                },
+                "title": "EKS:Clusters.NumberOfNodes",
+                "region": "eu-central-1",
+                "liveData": false,
+                "start": "-PT1H",
+                "end": "PT0H",
+                "view": "singleValue",
+                "stacked": false
+            }
+        },
+        {
+            "height": 6,
+            "width": 12,
+            "y": 12,
+            "x": 12,
+            "type": "metric",
+            "properties": {
+                "region": "eu-central-1",
+                "title": "Pods Network",
+                "legend": {
+                    "position": "right"
+                },
+                "timezone": "Local",
+                "metrics": [
+                    [ { "id": "expr0m0", "label": "prod epamapp-back-prod pod_network_rx_bytes", "expression": "mm0m0 + mm0farm0", "stat": "Average", "yAxis": "left" } ],
+                    [ { "id": "expr0m1", "label": "prod epamapp-front-prod pod_network_rx_bytes", "expression": "mm0m1 + mm0farm1", "stat": "Average", "yAxis": "left" } ],
+                    [ "ContainerInsights", "pod_network_rx_bytes", "PodName", "epamapp-back-prod", "Namespace", "prod", "ClusterName", "epam-py-cluster", { "id": "mm0m0", "visible": false, "yAxis": "left" } ],
+                    [ "ContainerInsights", "pod_network_rx_bytes", "PodName", "epamapp-front-prod", "Namespace", "prod", "ClusterName", "epam-py-cluster", { "id": "mm0m1", "visible": false, "yAxis": "left" } ],
+                    [ "ContainerInsights", "pod_network_rx_bytes", "ClusterName", "epam-py-cluster", "PodName", "epamapp-back-prod", "Namespace", "prod", "LaunchType", "fargate", { "id": "mm0farm0", "visible": false, "yAxis": "left" } ],
+                    [ "ContainerInsights", "pod_network_rx_bytes", "ClusterName", "epam-py-cluster", "PodName", "epamapp-front-prod", "Namespace", "prod", "LaunchType", "fargate", { "id": "mm0farm1", "visible": false, "yAxis": "left" } ],
+                    [ { "id": "expr1m0", "label": "prod epamapp-back-prod pod_network_tx_bytes", "expression": "mm1m0 + mm1farm0", "stat": "Average", "yAxis": "right" } ],
+                    [ { "id": "expr1m1", "label": "prod epamapp-front-prod pod_network_tx_bytes", "expression": "mm1m1 + mm1farm1", "stat": "Average", "yAxis": "right" } ],
+                    [ "ContainerInsights", "pod_network_tx_bytes", "PodName", "epamapp-back-prod", "Namespace", "prod", "ClusterName", "epam-py-cluster", { "id": "mm1m0", "visible": false, "yAxis": "right" } ],
+                    [ "ContainerInsights", "pod_network_tx_bytes", "PodName", "epamapp-front-prod", "Namespace", "prod", "ClusterName", "epam-py-cluster", { "id": "mm1m1", "visible": false, "yAxis": "right" } ],
+                    [ "ContainerInsights", "pod_network_tx_bytes", "ClusterName", "epam-py-cluster", "PodName", "epamapp-back-prod", "Namespace", "prod", "LaunchType", "fargate", { "id": "mm1farm0", "visible": false, "yAxis": "right" } ],
+                    [ "ContainerInsights", "pod_network_tx_bytes", "ClusterName", "epam-py-cluster", "PodName", "epamapp-front-prod", "Namespace", "prod", "LaunchType", "fargate", { "id": "mm1farm1", "visible": false, "yAxis": "right" } ]
+                ],
+                "start": "-P0DT1H0M0S",
+                "end": "P0D",
+                "liveData": false,
+                "period": 60,
+                "view": "timeSeries",
+                "stacked": false
+            }
+        },
+        {
+            "height": 3,
+            "width": 6,
+            "y": 3,
+            "x": 18,
+            "type": "metric",
+            "properties": {
+                "region": "eu-central-1",
+                "title": "Number of Pods",
+                "legend": {
+                    "position": "bottom"
+                },
+                "timezone": "Local",
+                "metrics": [
+                    [ "ContainerInsights", "node_number_of_running_pods", "InstanceId", "i-041c900efac984628", "NodeName", "ip-10-0-0-164.eu-central-1.compute.internal", "ClusterName", "epam-py-cluster", { "stat": "Average" } ]
+                ],
+                "start": "-P0DT1H0M0S",
+                "end": "P0D",
+                "liveData": false,
+                "period": 60,
+                "view": "singleValue",
+                "stacked": false
+            }
+        },
+        {
+            "height": 6,
+            "width": 12,
+            "y": 6,
+            "x": 0,
+            "type": "metric",
+            "properties": {
+                "region": "eu-central-1",
+                "title": "Node CPU Utilization",
+                "legend": {
+                    "position": "right"
+                },
+                "timezone": "Local",
+                "metrics": [
+                    [ "ContainerInsights", "pod_cpu_utilization", "PodName", "epamapp-front-prod", "Namespace", "prod", "ClusterName", "epam-py-cluster", { "stat": "Average" } ],
+                    [ "ContainerInsights", "pod_cpu_utilization", "PodName", "epamapp-back-prod", "Namespace", "prod", "ClusterName", "epam-py-cluster", { "stat": "Average" } ],
+                    [ "ContainerInsights", "pod_cpu_utilization", "PodName", "epamapp-front-dev", "Namespace", "dev", "ClusterName", "epam-py-cluster", { "stat": "Average" } ],
+                    [ "ContainerInsights", "pod_cpu_utilization", "PodName", "epamapp-back-dev", "Namespace", "dev", "ClusterName", "epam-py-cluster", { "stat": "Average" } ]
+                ],
+                "start": "-P0DT1H0M0S",
+                "end": "P0D",
+                "liveData": false,
+                "period": 60,
+                "view": "timeSeries",
+                "stacked": false
+            }
+        },
+        {
+            "height": 6,
+            "width": 12,
+            "y": 6,
+            "x": 12,
+            "type": "metric",
+            "properties": {
+                "region": "eu-central-1",
+                "title": "Node Memory Utilization",
+                "legend": {
+                    "position": "right"
+                },
+                "timezone": "Local",
+                "metrics": [
+                    [ "ContainerInsights", "pod_memory_utilization", "PodName", "epamapp-back-prod", "Namespace", "prod", "ClusterName", "epam-py-cluster", { "stat": "Average" } ],
+                    [ "ContainerInsights", "pod_memory_utilization", "PodName", "epamapp-front-prod", "Namespace", "prod", "ClusterName", "epam-py-cluster", { "stat": "Average" } ],
+                    [ "ContainerInsights", "pod_memory_utilization", "PodName", "epamapp-front-dev", "Namespace", "dev", "ClusterName", "epam-py-cluster", { "stat": "Average" } ],
+                    [ "ContainerInsights", "pod_memory_utilization", "PodName", "epamapp-back-dev", "Namespace", "dev", "ClusterName", "epam-py-cluster", { "stat": "Average" } ]
+                ],
+                "start": "-P0DT1H0M0S",
+                "end": "P0D",
+                "liveData": false,
+                "period": 60,
+                "view": "timeSeries",
+                "stacked": false
+            }
+        },
+        {
+            "type": "metric",
+            "x": 0,
+            "y": 12,
+            "width": 6,
+            "height": 3,
+            "properties": {
+                "metrics": [
+                    [ "AWS/RDS", "DatabaseConnections", "DBInstanceIdentifier", "db", { "period": 300, "stat": "Sum" } ]
+                ],
+                "legend": {
+                    "position": "bottom"
+                },
+                "region": "eu-central-1",
+                "liveData": false,
+                "start": "-PT1H",
+                "end": "PT0H",
+                "title": "DatabaseConnections: Sum",
+                "view": "singleValue",
+                "stacked": false
+            }
+        },
+        {
+            "type": "metric",
+            "x": 6,
+            "y": 12,
+            "width": 6,
+            "height": 6,
+            "properties": {
+                "metrics": [
+                    [ "AWS/RDS", "FreeableMemory", "DBInstanceIdentifier", "db", { "period": 300, "stat": "Average" } ]
+                ],
+                "legend": {
+                    "position": "bottom"
+                },
+                "region": "eu-central-1",
+                "liveData": false,
+                "start": "-PT1H",
+                "end": "PT0H",
+                "title": "Database FreeableMemory: Average",
+                "view": "timeSeries",
+                "stacked": true
+            }
+        }
+    ]
+}
+
+EOF
+}
