c8ccad3472a4d06d4ceb107c1146ef3efff9981e
runalsh <dev@runalsh.ru>
2022-04-26 16:03:28 +0300

    v2

--- back/app_test.py
+++ back/app_test.py
@@ -2,6 +2,6 @@ from app import app
 import pytest
 
 def test_answer():
-    response = app.test_client().get('http://localhost:8080/')
+    response = app.test_client().get('http://localhost:8080/back/ping')
 
     assert response.status_code == 200
