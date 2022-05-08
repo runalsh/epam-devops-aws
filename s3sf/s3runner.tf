7aa701b5132439d1a6d4cbe34c5cf8ce054fd1a9
runalsh <dev@runalsh.ru>
2022-05-08 09:17:55 +0300

    added runner provisioning

---
+++ s3sf/s3runner.tf
@@ -0,0 +1,34 @@
+#==== prov ======================
+
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.region
+}
+
+#===== variables =================
+
+variable "region" {
+  default = "eu-central-1"
+}
+
+#========== S3 ==============
+
+resource "aws_s3_bucket" "terraform_state" {
+   bucket = "statebucket-myy"
+   lifecycle {
+     prevent_destroy = true
+   }
+    versioning {
+      enabled = true
+    }
+ } 
+
+
+
